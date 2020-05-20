05f84b219db25d73496628058af3dc51073e4269
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-05-20 09:02:07 +0800

    feature
    添加反射技术项目

---
+++ Reflections/Reflections/Metadata/ObjectExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflections.Metadata
+{
+    /// <summary>
+    /// 在控制台打印给定对象的元数据描述。
+    /// 该方法用于调试和开发期间，对对象的运行时状态进行观察。
+    /// </summary>
+    public class ObjectExtension
+    {
+        public static void GetPropertyInfo(object obj)
+        {
+            var type = obj.GetType();
+
+            var props = type.GetProperties();
+            foreach (var prop in props)
+                Console.WriteLine($"{prop.Name}: {prop.GetValue(obj)}");
+        }
+    }
+}
