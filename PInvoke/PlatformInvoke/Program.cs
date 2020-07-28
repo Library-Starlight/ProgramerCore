5ad17497a81a2909ee84ad55c1a9a61ee05e8524
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-07-28 15:34:35 +0800

    feature
    函数顺序定义错误会发生Segmentation fault (core dumped)

--- PInvoke/PlatformInvoke/Program.cs
+++ PInvoke/PlatformInvoke/Program.cs
@@ -21,14 +21,15 @@ namespace PlatformInvoke
 
         public struct StatusCallback
         {
-            public delegate void online_callback_del();
-            public online_callback_del online_callback;
-
             public delegate void extra_callback_del1(int v, int y);
             public extra_callback_del1 extra_callback1;
 
-            public delegate void extra_callback_del2();
-            public extra_callback_del2 extra_callback2;
+            public delegate void online_callback_del();
+            public online_callback_del online_callback;
+
+
+            //public delegate void extra_callback_del2();
+            //public extra_callback_del2 extra_callback2;
         }
 
         [DllImport("demo0.so")]
