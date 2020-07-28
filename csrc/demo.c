19cee8491bfc5715a3f4714ffffd81a83564845e
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-07-28 17:03:17 +0800

    feature
    查找回调函数数量

--- csrc/demo.c
+++ csrc/demo.c
@@ -31,9 +31,22 @@ void Invoke(status_enum status)
 typedef struct StatusCallback
 {
     void (*online_callback)(uint32_t value);
+    void (*ext_callback1)(uint32_t value);
+    void (*ext_callback2)(uint32_t value);
+    void (*ext_callback3)(uint32_t value);
+    void (*ext_callback4)(uint32_t value);
+    void (*ext_callback5)(uint32_t value);
+    void (*ext_callback6)(uint32_t value);
+    void (*ext_callback7)(uint32_t value);
+    void (*ext_callback8)(uint32_t value);
+    void (*ext_callback9)(uint32_t value);
 } StatusCallback;
 
 void SetCallback(StatusCallback callback)
 {
-    // callback.online_callback(500);
+    callback.online_callback(500);
+    callback.ext_callback9(500);
+    // callback.ext_callback1(100);
+    // callback.ext_callback2(200);
+    // callback.ext_callback3(300);
 }
