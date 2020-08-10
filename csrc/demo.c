4e18c6ea38735caae4600365f2973a05de116c4b
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-08-10 18:05:16 +0800

    feat：pb例子

--- csrc/demo.c
+++ csrc/demo.c
@@ -1,17 +1,75 @@
 #include "stdio.h"
 #include "HeyThings_interface.h"
 #include "HeyThings_log.h"
+#include "devInfo.pb-c.h"
 
 int main(void)
 {
-    printf("libHeythings version: %d.%d.%d\n",
-           HEYTHINGS_VERSION_MAJOR, HEYTHINGS_VSERION_MINOR,
-           HEYTHINGS_VERSION_PATCH);
+    Pack();
 
-    uint64_t v = *(uint64_t *)"value";
-    printf(v);
+    // printf("libHeythings version: %d.%d.%d\n",
+    //        HEYTHINGS_VERSION_MAJOR, HEYTHINGS_VSERION_MINOR,
+    //        HEYTHINGS_VERSION_PATCH);
+
+    // uint64_t v = *(uint64_t *)"value";
+    // printf(v);
 
     return 0;
+}
+
+struct _Iot__Service__DevInfo__Properties
+{
+    ProtobufCMessage base;
+    char *devname;
+    char *manufacturer;
+    char *model;
+    char *brand;
+    char *softver;
+    uint64_t did;
+    uint32_t pid;
+    uint32_t cid;
+    char *mac;
+    char *hardver;
+    char *sn;
+    uint64_t parentdid;
+    int32_t rssi;
+    char *bssid;
+    char *ip;
+    char *devtime;
+    uint64_t utc;
+    char *timezone;
+    char *ssid;
+    /*
+   *用于上传云端进行家庭签名
+   */
+    ProtobufCBinaryData devpubkey;
+    /*
+   *dev identify in cloud
+   */
+    char *vendordevid;
+    Iot__Service__DevInfo__DevCapability *devcapability;
+    /*
+   *是否在线，0-离线, 1-在线
+   */
+    protobuf_c_boolean online;
+};
+
+typedef struct _Iot__Service__DevInfo__Properties Iot__Service__DevInfo__Properties;
+
+size_t iot__service__dev_info__properties__pack(const Iot__Service__DevInfo__Properties *message,
+                                                uint8_t *out)
+{
+    assert(message->base.descriptor == &iot__service__dev_info__properties__descriptor);
+    return protobuf_c_message_pack((const ProtobufCMessage *)message, out);
+}
+
+void Pack()
+{
+    Iot__Service__DevInfo__Properties props = IOT__SERVICE__DEV_INFO__PROPERTIES__INIT;
+
+    props.online = 1;
+    
+    iot__service__dev_info__properties__pack(&props, buf);
 }
 
 void Print()
