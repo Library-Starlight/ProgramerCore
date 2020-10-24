fa174cafb80bf00429c164f687c40cfcac00b387
Qz8954167 <357592895@qq.com>
2020-10-24 17:18:31 +0800

    feature
    二分查找算法

---
+++ Algorithms/Algorithms/Mathmatic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    /// <summary>
+    /// 数学
+    /// </summary>
+    public class Mathmatic
+    {
+        /// <summary>
+        /// 最大公约数
+        /// </summary>
+        /// <param name="p">非负整数1</param>
+        /// <param name="q">非负整数2</param>
+        /// <returns></returns>
+        public static uint Gcd(uint p, uint q)
+        {
+            if (q == 0) return p;
+            uint r = p % q;
+            return Gcd(q, r);
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Search/BinarySearch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.Search
+{
+    /// <summary>
+    /// 二分查找法
+    /// </summary>
+    public class BinarySearch
+    {
+        /// <summary>
+        /// 查找
+        /// </summary>
+        /// <param name="key">查找的数字</param>
+        /// <param name="a">有序数组</param>
+        /// <returns></returns>
+        public static int Rank(int key, int[] a)
+        {
+            int lo = 0;
+            int hi = a.Length - 1;
+            while (lo <= hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+                if (key < a[mid])      hi = mid - 1;
+                else if (key > a[mid]) lo = mid + 1;
+                else                   return mid;
+            }
+
+            return -1;
+        }
+
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine(Math.Abs(-2147483648));
+
+            //int i;
+            //Console.WriteLine(i);
+
+            //string s1 = "1";
+            //string s2 = "2";
+            //Console.WriteLine(s1 > s2);
+
+            int[] values = args
+                .Take(3)
+                .Select(s => int.Parse(s))
+                .ToArray();
+
+            if (values[0] == values[1] && values[1] == values[2])
+                Console.WriteLine("equal");
+            else
+                Console.WriteLine("not equal");
+        }
+    }
+}
