dd4ad05399b45b3d4ebce75ed0018b623c6bf1ae
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-10-25 23:15:58 +0800

    feature

---
+++ Algorithms/Algorithms/Practice/Chapter1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.Practice
+{
+    public class Chapter1
+    {
+        public void P1_1_5(string[] args)
+        {
+            int[] values = args
+                .Take(3)
+                .Select(s => int.Parse(s))
+                .ToArray();
+
+            if (values[0] == values[1] && values[1] == values[2])
+                Console.WriteLine("equal");
+            else
+                Console.WriteLine("not equal");
+        }
+
+        public void P1_1_6()
+        {
+            int f = 0;
+            int g = 1;
+            for (int i = 0; i <= 15; i++)
+            {
+                Console.WriteLine(f);
+                f = f + g;
+                g = f - g;
+            }
+        }
+
+        public void P1_1_7()
+        {
+            // a
+            //double t = 9.0;
+            //while (Math.Abs(t - 9.0 / t) > 0.001)
+            //{
+            //    t = (t + 9.0 / t) / 2.0;
+            //    //Console.WriteLine(t);
+            //}
+            //Console.WriteLine(t.ToString("0.00000"));
+
+            // b
+            //int sum = 0;
+            //for (int i = 1; i < 1000; i++)
+            //    for (int j = 0; j < i; j++)
+            //        sum++;
+            //Console.WriteLine(sum);
+
+            // c
+            int sum = 0;
+            for (int i = 1; i < 1000; i *= 2)
+                for (int j = 0; j < i; j++)
+                    sum++;
+            Console.WriteLine(sum);
+        }
+
+        public void P1_1_8()
+        {
+            Console.WriteLine('b');
+
+            Console.WriteLine('b' + 'c');
+
+            Console.WriteLine((char)('a' + 4));
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,15 @@
+﻿using Algorithms.Practice;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Chapter1().P1_1_8();
+        }
+    }
+}
