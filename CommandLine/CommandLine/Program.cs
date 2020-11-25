a32467d041db2727bc3f3b5ae65130e60ad54702
chenjilan123 <30375990+chenjilan123@users.noreply.github.com>
2020-11-25 17:47:14 +0800

    feature
    命令行程序

---
+++ CommandLine/CommandLine/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var argument in args)
+                Console.WriteLine(argument);
+        }
+    }
+}
